864889ee0a0a6da1a12e33524a7888e4a4364f74
Daniel <daniel.olayam@gmail.com>
2018-03-22 14:49:41 -0700

    single source file multiple binaries

--- diskinfo.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISKINFO_H_
-#define DISKINFO_H_
-
-extern unsigned int bsize, bcount, fatstart, fatblocks, rootstart, rootblocks;
-
-extern void intializeVars(unsigned char** waka);
-
-#endif
